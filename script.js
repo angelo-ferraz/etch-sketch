c264e7bc7e7cb13a15ecf02b84e6ee603b12c7a4
angelo-ferraz <104265131+angelo-ferraz@users.noreply.github.com>
2022-05-26 11:26:59 +0100

    added highlighting & initial mobile support

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ const sliderText = document.getElementById("slidertext");
 const gridSlider = document.getElementById("gridsizeslider");
 
 let gridRange = 18;
-let penColor = "black";
+let toggle = 0;
+let penColor = 'rgb(0,0,0)';
+
 document.onload = createCells(gridRange);
 document.onload = changeColor(penColor);
-
 
 // Setting Grid Size 
 function createCells (gridRange) {
     for (c=1; c <= gridRange*gridRange; c++){
         cell = document.createElement('div');
         cell.classList.add('cells');
+        cell.style.border = "1px red groove";
         gameArea.appendChild(cell);
+        toggle = 0;
     }
     adjustGrid();
 }
@@ -51,33 +54,46 @@ gridSlider.addEventListener('input', function(e) {
 
 // Coloring
 colorKnob.addEventListener('input', e => {
-    console.log(colorKnob.value);
-    penColor = colorKnob.value;
-    changeColor(penColor);
+    changeColor(colorKnob.value);
 })
 
 function changeColor(penColor) {
-    const babydivs = document.querySelectorAll('#gamearea > div');
+    const babydivs = document.querySelectorAll('.cells');
     babydivs.forEach(baby => {
-        baby.addEventListener('click', e => {
-            e.target.style.background = penColor;});
+        baby.addEventListener('mousedown', (e) => {
+            e.target.style.background = penColor;
+            });
+        // Highlight cursor cell
         baby.addEventListener('mouseenter', (e) => {
-            e.target.style.opacity = 0.2;});
-        baby.addEventListener('mouseleave', (e) => {
-            e.target.style.opacity = 1;});
+            e.target.style.border = `1px solid black`;
+            });
+        baby.addEventListener('mouseout', (e) => {
+            if (toggle === 0) {
+                e.target.style.border = '1px red groove';}
+            else {e.target.style.border = 'none';}
+        });
+        // Mobile Support
+        baby.addEventListener('ontouchmove', (e) => {
+            e.target.style.background = penColor;
+            e.target.style.border = `1px solid black`;
+        });
+        baby.addEventListener('ontouchend', (e) => {
+            if (toggle === 0) {
+                e.target.style.border = '1px red groove';}
+            else {e.target.style.border = 'none';}
+        });
     });
 }
 
 // Reset Button
 resetKnob.addEventListener('click', e => {
     babydiv = document.getElementsByClassName('cells');
-    for (b = 0; b < babydiv.length; b++){babydiv[b].style.background = 'white';}
+    for (b = 0; b < babydiv.length; b++){babydiv[b].style.background = 'rgb(255,255,255)';}
 }); 
 
-// Hide grid lines when clicking XxX
-let toggle = 0
+// Hide grid lines when clicking 18x18 text
 sliderText.addEventListener('mouseenter', e => {
-    toggletext = document.getElementById('tickingbomb');
+    toggletext = document.getElementById('hideme');
     toggletext.innerText = '';
 });
 
@@ -86,9 +102,11 @@ sliderText.addEventListener('click', e => {
     switch (toggle) {
         case 0:
             for (b = 0; b < babydiv.length; b++){babydiv[b].style.border = 'none'; toggle = 1};
+            console.log(toggle);
             break;
         case 1:
             for (b = 0; b < babydiv.length; b++){babydiv[b].style.border = '1px red groove'; toggle = 0};
+            console.log(toggle);
             break;
     }
 });
